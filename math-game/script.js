b9cdf07ac7ca8956e24942dac15d0cbf0ebdf799
Jonathan Almeida <almeidajonathaan@gmail.com>
2022-09-04 23:28:43 -0300

    add verifications and validations to buttons

--- math-game/script.js
+++ math-game/script.js
@@ -1,61 +1,101 @@
-const startBtn = document.getElementById("start-btn")
-const question = document.getElementById("question")
-const controls = document.querySelector(".controls-container")
-const result = document.getElementById("result")
-const submitBtn = document.getElementById("submit-btn")
-const errorMessage = document.getElementById("error-msg")
+const startBtn = document.getElementById("start-btn");
+const question = document.getElementById("question");
+const controls = document.querySelector(".controls-container");
+const result = document.getElementById("result");
+const submitBtn = document.getElementById("submit-btn");
+const errorMessage = document.getElementById("error-msg");
+const body = document.querySelector("body");
 
-const operators = ["+","-","*"]
-let answerValue
-let operatorQuestion
+const operators = ["+", "-", "*"];
+let answerValue;
+let operatorQuestion;
 
 // Random Value Generator
-const randomValue = (min, max) => Math.floor(Math.random() * (max - min)) + min
+const randomValue = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
 const questionGenerator = () => {
-    // Two random values between 1 and 20
-    let [num1, num2] = [randomValue(1, 20), randomValue(1, 20)]
-    console.log(num1, num2)
+  // Two random values between 1 and 20
+  let [num1, num2] = [randomValue(1, 20), randomValue(1, 20)];
+  console.log(num1, num2);
 
-    // For getting random operator
-    let randomOperator = operators[Math.floor(Math.random() * operators.length)]
-    console.log(randomOperator) 
+  // For getting random operator
+  let randomOperator = operators[Math.floor(Math.random() * operators.length)];
+  console.log(randomOperator);
 
-    if(randomOperator == "-" && num2 > num1) {
-        [num1, num2] = [num2, num1];
-    }
+  if (randomOperator == "-" && num2 > num1) {
+    [num1, num2] = [num2, num1];
+  }
 
-    // Solve equation
-    let solution = eval(`${num1} ${randomOperator} ${num2}`)
-    console.log(num1, randomOperator, num2, solution)
+  // Solve equation
+  let solution = eval(`${num1} ${randomOperator} ${num2}`);
+  console.log(num1, randomOperator, num2, solution);
 
-    // For placing the input at random position
-    let randomVar = randomValue(1, 5)
-    console.log(randomVar)
+  // For placing the input at random position
+  let randomVar = randomValue(1, 5);
+  console.log(randomVar);
 
-    if(randomVar == 1) {
-        answerValue = num1
-        question.innerHTML = `<input type="number" id="inputValue" placeholder="?"/> ${randomOperator} ${num2} = ${solution}`
-    }
-    else if(randomVar == 2) {
-        answerValue = num2
-        question.innerHTML = `${num1} ${randomOperator} <input type="number" id="inputValue" placeholder="?"/> = ${solution}`
+  if (randomVar == 1) {
+    answerValue = num1;
+    question.innerHTML = `<input type="number" id="inputValue" placeholder="?"/> ${randomOperator} ${num2} = ${solution}`;
+  } else if (randomVar == 2) {
+    answerValue = num2;
+    question.innerHTML = `${num1} ${randomOperator} <input type="number" id="inputValue" placeholder="?"/> = ${solution}`;
+  } else if (randomVar == 3) {
+    answerValue = randomOperator;
+    operatorQuestion = true;
+    question.innerHTML = `${num1} <input type="text" id="inputValue" placeholder="?"/> ${num2} = ${solution}`;
+  } else {
+    answerValue = solution;
+    question.innerHTML = `${num1} ${randomOperator} ${num2} = <input type="number" id="inputValue" placeholder="?"/>`;
+  }
+};
+// User Input Check
+submitBtn.addEventListener("click", () => {
+  errorMessage.classList.add("hide");
+  let userInput = document.getElementById("inputValue").value;
+
+  //If user input is not empty
+  if (userInput) {
+    //If the user guessed correct answer
+    if (userInput == answerValue) {
+      body.style.backgroundColor = "green";
+      stopGame(`Yippie!! <span>Correct</span> Answer`);
     } 
-
-    else if(randomVar == 3) {
-        answerValue = randomOperator
-        operatorQuestion = true
-        question.innerHTML = `${num1} <input type="number" id="inputValue" placeholder="?"/> ${num2} = ${solution}`
-    }
-
-
+    //If user inputs operator other than +,-,* 
+    else if (operatorQuestion && !operators.includes(userInput)) {
+      errorMessage.classList.remove("hide");
+      errorMessage.innerHTML = "Please enter a valid operator";
+    } 
+    //If user guessed wrong answer
     else {
-        answerValue = solution
-        question.innerHTML = `${num1} ${randomOperator} ${num2} = <input type="number" id="inputValue" placeholder="?"/>`
+      body.style.backgroundColor = "red";
+      stopGame(`Opps!!<span>Wrong</span> Answer`);
     }
-}
+  } 
+  //
+  else {
+    errorMessage.classList.remove("hide");
+    errorMessage.innerHTML = "Input Cannot Be Empty"
+  }
+});
 
 // Start Game
 startBtn.addEventListener("click", () => {
-    questionGenerator()
-})
+  body.style.backgroundColor = "#f4c531";
+  operatorQuestion = false;
+  answerValue = "";
+  errorMessage.innerHTML = "";
+  errorMessage.classList.add("hide");
+  //Controls and buttons visibility
+  controls.classList.add("hide");
+  startBtn.classList.add("hide");
+  questionGenerator();
+});
+
+//  Stop Game
+const stopGame = (resultText) => {
+  result.innerHTML = resultText;
+  startBtn.innerHTML = "Restart";
+  controls.classList.remove("hide");
+  startBtn.classList.remove("hide");
+};
